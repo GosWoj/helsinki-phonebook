323a762a0ab74a851790ac107ccd06fafd07fad5
GosWoj <gosiawoojcik@gmail.com>
2021-07-26 16:42:19 +0200

    step3 done - added number

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ const App = () => {
   const [persons, setPersons] = useState([
     {
       name: "Arto Hellas",
+      number: "040-1234567",
     },
   ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const person = {
       name: newName,
+      number: newNumber,
     };
     const samePerson = persons.find((p) => p.name === newName);
 
@@ -20,11 +23,16 @@ const App = () => {
     } else {
       setPersons(persons.concat(person));
       setNewName("");
+      setNewNumber("");
     }
   };
 
   const handleChange = (e) => {
     setNewName(e.target.value);
+  };
+
+  const handleNumber = (e) => {
+    setNewNumber(e.target.value);
   };
 
   return (
@@ -34,12 +42,19 @@ const App = () => {
         <div>
           Name: <input value={newName} onChange={handleChange} />
         </div>
+        <div style={{ padding: "0.5rem 0" }}>
+          Number: <input value={newNumber} onChange={handleNumber} />
+        </div>
         <button type="submit">Add</button>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => {
-          return <li key={person.name}>{person.name}</li>;
+          return (
+            <li key={person.name}>
+              {person.name} {person.number}
+            </li>
+          );
         })}
       </ul>
     </div>
