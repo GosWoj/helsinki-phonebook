219b9632535dbfa0d149afb25da5374ff0444d73
GosWoj <gosiawoojcik@gmail.com>
2021-07-26 15:43:06 +0200

    step1 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [persons, setPersons] = useState([
+    {
+      name: "Arto Hellas",
+    },
+  ]);
+  const [newName, setNewName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const person = {
+      name: newName,
+    };
+    setPersons(persons.concat(person));
+  };
+
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+
   return (
     <div>
-      <h1>h</h1>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Name: <input onChange={handleChange} />
+        </div>
+        <button type="submit">Add</button>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => {
+          return <li key={person.name}>{person.name}</li>;
+        })}
+      </ul>
+      <div>Debug: {newName}</div>
     </div>
   );
 };
